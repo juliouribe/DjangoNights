f8a6102d1c54c99d74572f968f6545dc01f49f47
Julio Uribe <uribejulioc@gmail.com>
2024-12-27 08:42:45 -0800

    Finish chapter 2

--- DjangoForBeginners/ch2-hellow-world/django_project/settings.py
+++ DjangoForBeginners/ch2-hellow-world/django_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+
 from .secrets import SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -35,6 +36,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "pages",
 ]
 
 MIDDLEWARE = [
