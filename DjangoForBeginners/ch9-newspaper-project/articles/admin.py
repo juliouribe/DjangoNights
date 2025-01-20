e938ec5836360d1dbd61084c4ef14da01a5d89ac
Julio Uribe <uribejulioc@gmail.com>
2025-01-20 14:27:11 -0800

    Finish chapter 15

--- DjangoForBeginners/ch9-newspaper-project/articles/admin.py
+++ DjangoForBeginners/ch9-newspaper-project/articles/admin.py
@@ -1,8 +1,21 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article, Comment
+
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 0
+
+
+# class CommentInline(admin.StackedInline):
+#     model = Comment
+#     extra = 0
 
 
 class ArticleAdmin(admin.ModelAdmin):
+    inlines = [
+        CommentInline,
+    ]
     list_display = [
         "title",
         "body",
@@ -11,3 +24,4 @@ class ArticleAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment)
